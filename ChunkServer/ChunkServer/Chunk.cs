1924973f041ce2e1a46e563eb7e8d0669f5279ec
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-06-25 18:37:45 +0200

    Initial Commit

---
+++ ChunkServer/ChunkServer/Chunk.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ChunkServer {
+    public class Chunk {
+        public int Id { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ ChunkServer/ChunkServer/Controllers/ChunkController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChunkServer.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChunkController : ControllerBase {
+        private readonly string _basePath;
+
+        public ChunkController(IConfiguration configuration)
+        {
+            _basePath = configuration.GetValue<string>("ChunkStoragePath");
+        }
+
+        [HttpPost("storeChunk")]
+        public async Task<IActionResult> StoreChunk([FromBody] Chunk chunk)
+        {
+            string filePath = Path.Combine(_basePath, chunk.Id.ToString());
+            try
+            {
+                await System.IO.File.WriteAllTextAsync(filePath, chunk.Data);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet("getChunk/{id}")]
+        public async Task<IActionResult> GetChunk(int id)
+        {
+            string filePath = Path.Combine(_basePath, id.ToString());
+            try
+            {
+                var content = await System.IO.File.ReadAllTextAsync(filePath);
+                return Ok(content);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpDelete("deleteChunk/{id}")]
+        public IActionResult DeleteChunk(int id)
+        {
+            string filePath = Path.Combine(_basePath, id.ToString());
+            try
+            {
+                System.IO.File.Delete(filePath);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
